91187e80fbc845acd4f4db095d5e7a2cf15e215c
Daniel Deane <daniel.michael.deane@gmail.com>
2023-11-03 16:49:47 -0500

    work in progress

---
+++ src/main/java/Album.java
@@ -0,0 +1,16 @@
+import java.io.Serializable;
+
+
+public class Album Serializable {
+
+    private long id;
+    private String Author_First_Name;
+    private String Author_Last_Name;
+
+
+
+public Album(long id, String Author_First_Name, String Author_Last_Name) {
+    this.id = id;
+    this.Author_First_Name = Author_First_Name;
+    this.Author_Last_Name = Author_Last_Name;
+}
---
+++ src/main/java/BeanTest.java
@@ -0,0 +1,8 @@
+public class BeanTest {
+
+
+    public static void main(String[] args) {
+
+    Album album = new Album("The Beatles", "Abbey Road", 1969, 17.99);
+    }
+}
